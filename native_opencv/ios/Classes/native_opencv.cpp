063ea2d6ed53b26aa37647962dcf3da8b0393088
linus saisi <42277372+saisilinus@users.noreply.github.com>
2021-04-10 15:42:37 +0300

    feat: add threshold segmentation

--- native_opencv/ios/Classes/native_opencv.cpp
+++ native_opencv/ios/Classes/native_opencv.cpp
@@ -34,6 +34,74 @@ extern "C" {
     }
 
     __attribute__((visibility("default"))) __attribute__((used))
+    bool compareContourAreas ( vector<Point> contour1, vector<Point> contour2 ) {
+        double i = fabs( contourArea(Mat(contour1)) );
+        double j = fabs( contourArea(Mat(contour2)) );
+        return ( i > j );
+    }
+
+    __attribute__((visibility("default"))) __attribute__((used))
+    int convertToInt (float num){
+        return static_cast<int>(num);
+    }
+
+    __attribute__((visibility("default"))) __attribute__((used))
+    void thresholdSegmentation(Mat input, Mat input_gray, char* outputImagePath){
+        Mat grad, threshed, blurred, kernel, closed;
+        vector<vector<Point>> contours;
+        vector<Point> largestContour;
+        vector<Vec4i> hierarchy;
+        RotatedRect rect;
+        vector<int> points;
+        //vector<int> intPoints;
+        int ddepth = CV_32F;
+
+        Mat grad_x, grad_y;
+        Mat abs_grad_x, abs_grad_y;
+        Sobel(input_gray, grad_x, ddepth, 1, 0, -1);
+        Sobel(input_gray, grad_y, ddepth, 0, 1, -1);
+        
+        // Convert output to a CV_8U image
+        convertScaleAbs(grad_x, abs_grad_x);
+        convertScaleAbs(grad_y, abs_grad_y);
+
+        //Gradient
+        addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+
+        // blurring
+        blur(grad, blurred, Size(9, 9));
+
+        // Binary thresholding
+        threshold(blurred, threshed, 230, 255, THRESH_BINARY);
+
+        // get kernel for morphological operations
+        kernel = getStructuringElement(MORPH_RECT, Size(21, 7));
+
+        // closing morphological op
+        morphologyEx( threshed, closed, MORPH_CLOSE, kernel );
+
+        // find contours
+        findContours(closed, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        //sort contours according to area
+        sort(contours.begin(), contours.end(), compareContourAreas);
+
+        // get largest contour
+        largestContour = contours[0];
+
+        rect = minAreaRect(largestContour);
+
+        boxPoints(rect, points);
+
+        // transform box point values to integers
+        //intPoints(points.begin(), points.end());
+
+        drawContours(input, points, -1, Scalar(0, 255, 0), 3);
+
+        imwrite(outputImagePath, input);
+    }
+
+    __attribute__((visibility("default"))) __attribute__((used))
     class ShapeDetector {
         public:
             string detectShape(std::vector<Point> contour){
@@ -60,26 +128,29 @@ extern "C" {
     void process_image(char* inputImagePath, char* outputImagePath) {
         long long start = get_now();
 
-        Mat input = imread(inputImagePath, IMREAD_GRAYSCALE);
+        Mat input = imread(inputImagePath);
+
+        Mat input_gray = imread(inputImagePath, IMREAD_GRAYSCALE);
         Mat threshed, withContours, blurred;
 
         vector<vector<Point>> contours;
         vector<Vec4i> hierarchy;
 
         // image preprocessing
-        GaussianBlur(input, blurred, Size(5, 5), 0);
-        threshold(blurred, threshed, 60, 255, THRESH_BINARY);
-        findContours(threshed, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+        // GaussianBlur(input, blurred, Size(5, 5), 0);
+        // threshold(blurred, threshed, 60, 255, THRESH_BINARY);
+        // findContours(threshed, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-        drawContours(threshed, contours, -1, Scalar(0, 255, 0), 4);
+        // drawContours(threshed, contours, -1, Scalar(0, 255, 0), 4);
 
-        ShapeDetector sd;
-        for(int i = 0; i < contours.size(); i++){
-            string shape = sd.detectShape(contours[i]);
-            platform_log("Shape: %s\n", shape.c_str());
-        }
+        // ShapeDetector sd;
+        // for(int i = 0; i < contours.size(); i++){
+        //     string shape = sd.detectShape(contours[i]);
+        //     platform_log("Shape: %s\n", shape.c_str());
+        // }
 
-        imwrite(outputImagePath, threshed);
+        // imwrite(outputImagePath, grad);
+        thresholdSegmentation(input, input_gray, outputImagePath);
         
         int evalInMillis = static_cast<int>(get_now() - start);
         platform_log("Processing done in %dms\n", evalInMillis);
